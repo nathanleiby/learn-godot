84fc6b377cc9526402b7c74ec159fd8bdff24a2c
Nathan Leiby <nathanleiby@gmail.com>
2021-08-03 21:57:08 -0700

    start adding Player logic

---
+++ src/Player/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+class_name Player  # creates a Player class
+
+# store player's dependencies
+onready var state_machine := $StateMachine
+onready var collider: CollisionShape2D = $CollisionShape2D
+
+const FLOOR_NORMAL := Vector2.UP  # we will use this with move_and_slide logic
+# we could change this later to allow player to walk on walls or the ceiling
+
+var is_active := true setget set_is_active
+
+
+# this will get called during init, before on_ready
+func set_is_active(value: bool) -> void:
+	is_active = value
+	if not collider:
+		return
+	collider.disabled = not value
